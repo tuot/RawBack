c080d9046fe43aae74b974d84bd9d30eea008062
levi <iooicoder@gmail.com>
2026-06-23 17:26:09 +0800

    Remove deprecated resources and files for RawBack extension

--- RawBack/Shared (App)/MacLifecycleDelegate.swift
+++
@@ -1,9 +0,0 @@
-#if os(macOS)
-import AppKit
-
-final class MacLifecycleDelegate: NSObject, NSApplicationDelegate {
-    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-        true
-    }
-}
-#endif
